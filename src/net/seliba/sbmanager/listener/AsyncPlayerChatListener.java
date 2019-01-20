3dc0c62bb088ed6600a3c75595b4fbf0d3ddd4e6
Seliba <munita.gavai@gmail.com>
2019-01-20 13:28:39 +0100

    Added debug messages

--- src/net/seliba/sbmanager/listener/AsyncPlayerChatListener.java
+++ src/net/seliba/sbmanager/listener/AsyncPlayerChatListener.java
@@ -12,11 +12,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerChatEvent;
 
 public class AsyncPlayerChatListener implements Listener {
 
   @EventHandler
-  public void onAsyncChat(AsyncPlayerChatEvent event) {
+  public void onChat(PlayerChatEvent event) {
     Player player = event.getPlayer();
 
     if(!AnswerManager.hasRequest(player)) {
@@ -25,19 +26,23 @@ public class AsyncPlayerChatListener implements Listener {
 
     switch(AnswerManager.getRequestType(player)) {
       case CREATION_CUSTOM_NAME:
+        System.out.println("1");
         AnswerManager.removeRequest(player);
         SchematicDataManager.addSchematicName(player, event.getMessage());
         SchematicManager.createCustomSchematic(player);
         break;
       case CREATION_WEB_NAME:
+        System.out.println("2");
         AnswerManager.removeRequest(player);
         SchematicDataManager.addSchematicName(player, event.getMessage());
         SchematicManager.createWebSchematic(player);
         break;
       case CREATION_WEB_WEBSITE:
+        System.out.println("3");
         AnswerManager.removeRequest(player);
         SchematicDataManager.addSchematicUrl(player, event.getMessage());
         AnswerManager.addRequest(player, AnswerType.CREATION_WEB_NAME);
+        player.sendMessage("§aBitte gebe den Namen des Schematics ein!");
         break;
     }
   }
